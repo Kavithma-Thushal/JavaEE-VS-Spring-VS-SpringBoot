c1e88036970b47dce66cd5e0b5f4d87eab66ce10
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-07-16 09:01:50 +0530

    Global Exception Handler Removed From Spring Project

--- Spring/src/main/java/lk/ijse/gdse66/spring/config/WebAppConfig.java
+++ Spring/src/main/java/lk/ijse/gdse66/spring/config/WebAppConfig.java
@@ -1,6 +1,5 @@
 package lk.ijse.gdse66.spring.config;
 
-import lk.ijse.gdse66.spring.advisor.GlobalExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  **/
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {GlobalExceptionHandler.class}, basePackages = "lk.ijse.gdse66.spring.api")
+@ComponentScan(basePackages = "lk.ijse.gdse66.spring.api")
 public class WebAppConfig {
 }
